f221f73d2aaab261774ebbbbed69469c8dd5ca39
leonardo <leomarcos.b10@gmail.com>
2025-10-29 15:14:11 -0300

    29-10-2024

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, { request, Response, NextFunction } from 'express'
+import express, {Request,Response,NextFunction } from 'express'
 import 'empress-async-errors'
 import cors from 'cors'
 
@@ -6,3 +6,18 @@ const app = express()
 app.use(express.json()) //Uma função dentro de um função
 
 app.use(cors())
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+        if(err instanceof Error){
+        return res.status(400).json({
+            error: err.message
+        })
+     }   
+    return res.status(500).json({
+        status: 'Erro',
+        message: 'Erro Interno do Servidor'
+    })
+})
+app.listen(3333, () => console.log('Servidor On-line na porta 3333'))
+    
+    
